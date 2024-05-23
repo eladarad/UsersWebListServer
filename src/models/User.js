fc94681d8bd88894b4c4c3c4123581e893e9dea3
Elad Arad <elad.arad@gmail.com>
2024-05-23 22:24:21 +0300

    implemented user model. Initial commit.

---
+++ src/models/User.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+  id: { type: Number, required: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: { type: String, required: true, unique: true }
+});
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
